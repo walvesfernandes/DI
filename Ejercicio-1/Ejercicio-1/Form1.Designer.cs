bf7857d7b619abb5daba0258ed406c72ed5284e3
walvesfernandes <walvesfernandes15@gmail.com>
2022-09-26 22:52:44 +0200

    DI-first

---
+++ Ejercicio-1/Ejercicio-1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Ejercicio_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.simbSuma = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(39, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // simbSuma
+            // 
+            this.simbSuma.AutoSize = true;
+            this.simbSuma.Location = new System.Drawing.Point(62, 37);
+            this.simbSuma.Name = "simbSuma";
+            this.simbSuma.Size = new System.Drawing.Size(13, 13);
+            this.simbSuma.TabIndex = 2;
+            this.simbSuma.Text = "+";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(126, 41);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(13, 13);
+            this.result.TabIndex = 3;
+            this.result.Text = "=";
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Location = new System.Drawing.Point(33, 60);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(75, 23);
+            this.btnSumar.TabIndex = 4;
+            this.btnSumar.Text = "Suma";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 123);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.simbSuma);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ejer1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label simbSuma;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Button btnSumar;
+    }
+}
+
---
+++ Ejercicio-1/Ejercicio-1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+
+            double resultDouble;
+
+            result.Text = "=";
+
+
+            if ((double.TryParse(textBox1.Text, out double lbl1)) && (double.TryParse(textBox2.Text, out double lbl2))) { 
+                resultDouble = Math.Truncate((Convert.ToDouble(textBox1.Text) + Convert.ToDouble(textBox2.Text)))/10; //hago un truncate para mostrar los decimales
+                result.Text = String.Format("{0}", resultDouble);
+            }
+            else
+            {
+                result.Text = "Debes inserir un numero";
+            }
+
+
+        }
+    }
+}
